eda7e3e32b6c921f4283fc62dcea0c3ece876eff
Apprentice <apprentice@dbc24.local>
2013-09-18 11:08:51 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaptainsLog::Application.config.secret_token = '936103e102bc477389479220f93c79d9047e066a616d84bf75d65d437f90a2419b2248e5a2fa8dbfb6e9f36d843e2e3135a1d6e5535902d906629cd491870da4'
