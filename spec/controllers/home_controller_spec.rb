2b2fadfc11e66b002685c43a54a3cf64d2cecc31
Apprentice <apprentice@dbc24.local>
2013-09-18 15:17:49 -0700

    created route and view for index

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+
+describe HomeController do
+  describe "homepage" do
+    it "exists" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end 
+end 
