2b131ff7ed5353ca878fee332724f0ac8c5e00af
Apprentice <apprentice@dbc21.local>
2013-09-18 15:24:38 -0700

    add bcrypt to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+
   attr_accessible :name, :email
   has_many :questions
   has_many :answers
+
+  include BCrypt
+
+  def password
+      @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
