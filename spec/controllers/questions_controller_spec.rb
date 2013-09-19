1d7f852acd2ad6fbeb08c9a5bdc81baea326f474
Apprentice <apprentice@dbc24.local>
2013-09-18 20:14:57 -0700

    add tests for new & create question routes

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe QuestionsController do
-  describe "show" do
-    let!(:user) { User.create(name: "Nick", email: "nick@nick.com") }
+  let!(:user) { User.create(name: "Nick", email: "nick@nick.com") }
     let!(:test_question) { Question.create(title: "Dogs", description: "Dogs are the best", user: user) }
     let!(:test_answer1) { Answer.create(description: "I hate your question.", user: user, question: test_question) }
     let!(:test_answer2) { Answer.create(description: "I hate your question too.", user: user, question: test_question) }
+
+  describe "show" do
+
     it "exists" do
       request_params = {id: test_question.id}
       get :show, request_params
@@ -23,6 +25,25 @@ describe QuestionsController do
       get :show, request_params
       expect(assigns(:answers)).to eq([test_answer1, test_answer2])
     end
+
+  end
+
+  describe "new" do
+
+    it "exists" do
+      get :new
+      expect(response.status).to eq(200)
+    end
+
+    it "should check to see if a user is logged in"
+
+  end
+
+  describe "create" do
+
+    it "exists"
+
+    it "creates a new question"
 
   end
 
